ed2dca432b66bb010de89009373fb98aacfd6f7a
Milena Yamamoto <milymanamy3@gmail.com>
2024-05-27 17:01:37 +0900

    .

--- aulas/GUI/aula255calculadora/main.py
+++ aulas/GUI/aula255calculadora/main.py
@@ -4,10 +4,20 @@ from main_window import MainWindow
 from PySide6.QtWidgets import QApplication, QLabel
 from PySide6.QtGui import QIcon
 from variables import WINDOW_ICON_PATH
+from display import Display
+from info import Info
+from styles import setupTheme
+from buttons import Button
 
 if __name__ == "__main__":
-    # Cria a aplicação
+    # nomenclaturas:
+    # snake_case
+    # PascalCase
+    # camelCase
+
+    # Cria a aplicação com tema
     app = QApplication(sys.argv)
+    setupTheme()
     window = MainWindow()
 
     # Define o ícone
@@ -15,10 +25,25 @@ if __name__ == "__main__":
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
 
+    # Info
+    info = Info("2.0 ^ 10.0 = 1024")
+    window.addToVLayout(info)
+
+    # Display
+    display = Display()  # if u put a text inside, it will be printed
+    display.setPlaceholderText("Digite aqui")  # fica transparente
+    window.addToVLayout(display)
+
+    button = Button("Texto do botão")
+    window.addToVLayout(button)
+
+    button2 = Button("Texto do botão")
+    window.addToVLayout(button2)
+
     # Exemplo de addWidget com QLabel
-    label1 = QLabel("O meu texto")
-    label1.setStyleSheet("font-size: 150px;")
-    window.addWidgetToVLayout(label1)
+    # label1 = QLabel("O meu texto")
+    # label1.setStyleSheet("font-size: 150px;")
+    # window.addToVLayout(label1)
 
     # Executa tudo
     window.adjustFixedSize()
---
+++ aulas/GUI/aula255calculadora/styles.py
@@ -0,0 +1,40 @@
+# QSS - Estilos do QT for Python
+# https://doc.qt.io/qtforpython/tutorials/basictutorial/widgetstyling.html
+
+# Dark Theme - How to use
+# https://pyqtdarktheme.readthedocs.io/en/latest/how_to_use.html
+
+import qdarktheme
+from variables import DARKER_PRIMARY_COLOR, DARKEST_PRIMARY_COLOR, PRIMARY_COLOR
+
+# color: #fff -> branco
+qss = f"""
+    QPushButton[cssClass="specialButton"] {{
+        color: #fff; 
+        background: {PRIMARY_COLOR};
+    }}
+    QPushButton[cssClass="specialButton"]:hover {{
+        color: #fff;
+        background: {DARKER_PRIMARY_COLOR};
+    }}
+    QPushButton[cssClass="specialButton"]:pressed {{
+        color: #fff;
+        background: {DARKEST_PRIMARY_COLOR};
+    }}
+"""
+
+
+def setupTheme():
+    qdarktheme.setup_theme(
+        theme="dark",
+        corner_shape="rounded",
+        custom_colors={
+            "[dark]": {
+                "primary": f"{PRIMARY_COLOR}",
+            },
+            "[light]": {
+                "primary": f"{PRIMARY_COLOR}",
+            },
+        },
+        additional_qss=qss,
+    )
