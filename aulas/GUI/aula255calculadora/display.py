d16bc970a2ec23b27087ba0ae3771cc13250f590
Milena Yamamoto <milymanamy3@gmail.com>
2024-06-06 17:32:43 +0900

    .

--- aulas/GUI/aula255calculadora/display.py
+++ aulas/GUI/aula255calculadora/display.py
@@ -1,9 +1,12 @@
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, Signal
 from PySide6.QtWidgets import QLineEdit
+from PySide6.QtGui import QKeyEvent
 from variables import BIG_FONT_SIZE, MINIMUM_WIDTH, TEXT_MARGIN
 
 
 class Display(QLineEdit):
+    eqRequested = Signal()
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.configStyle()
@@ -17,3 +20,19 @@ class Display(QLineEdit):
         self.setMinimumWidth(MINIMUM_WIDTH)  # largura min
         self.setAlignment(Qt.AlignmentFlag.AlignRight)  # cursor para a direita
         self.setTextMargins(*margins)  # asterisco tira da lista
+
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+
+        key = event.key()  # tecla
+        KEYS = Qt.Key
+
+        # print(event.text())  # valor digitado
+        # print(event.key())  # código do valor digitado
+
+        isEnter = key in [KEYS.Key_Enter, KEYS.Key_Return]
+
+        if isEnter:
+            print("Enter pressionado, sinal emitido", type(self).__name__)
+            self.eqRequested.emit()
+            return event.ignore()  # fazer nada com o enter
+        # return super().keyPressEvent(event)
