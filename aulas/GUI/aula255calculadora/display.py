0ac44d17f1f61d2858ed7f7b817e8361e82de15f
Milena Yamamoto <milymanamy3@gmail.com>
2024-06-07 20:45:01 +0900

    .

--- aulas/GUI/aula255calculadora/display.py
+++ aulas/GUI/aula255calculadora/display.py
@@ -2,14 +2,21 @@ from PySide6.QtCore import Qt, Signal
 from PySide6.QtWidgets import QLineEdit
 from PySide6.QtGui import QKeyEvent
 from variables import BIG_FONT_SIZE, MINIMUM_WIDTH, TEXT_MARGIN
+from utils import isEmpty, isNumOrDot
 
 
 class Display(QLineEdit):
-    eqRequested = Signal()
+    eqPressed = Signal()
+    delPressed = Signal()
+    clearPressed = Signal()
+    inputPressed = Signal(str)
+    operatorPressed = Signal(str)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.configStyle()
+
+        # self.setReadOnly(True)  # não deixa o usuário clicar em seu teclado
 
     def configStyle(self):
         margins = [
@@ -22,17 +29,59 @@ class Display(QLineEdit):
         self.setTextMargins(*margins)  # asterisco tira da lista
 
     def keyPressEvent(self, event: QKeyEvent) -> None:
-
+        text = event.text().strip()  # strip remove espaços dos lados
         key = event.key()  # tecla
         KEYS = Qt.Key
 
         # print(event.text())  # valor digitado
         # print(event.key())  # código do valor digitado
 
-        isEnter = key in [KEYS.Key_Enter, KEYS.Key_Return]
+        isEnter = key in [KEYS.Key_Enter, KEYS.Key_Return, KEYS.Key_Equal]
+        isDelete = key in [KEYS.Key_Backspace, KEYS.Key_Delete, KEYS.Key_D]
+        isEsc = key in [KEYS.Key_Escape, KEYS.Key_C]
+        isOperator = key in [
+            KEYS.Key_Plus,
+            KEYS.Key_Minus,
+            KEYS.Key_Slash,
+            KEYS.Key_Asterisk,
+            KEYS.Key_P,
+        ]
 
         if isEnter:
-            print("Enter pressionado, sinal emitido", type(self).__name__)
-            self.eqRequested.emit()
+            print(f"EQ {text} pressionado, sinal emitido", type(self).__name__)
+            self.eqPressed.emit()
             return event.ignore()  # fazer nada com o enter
         # return super().keyPressEvent(event)
+        if isDelete:
+            print(f"Delete {text} pressionado, sinal emitido", type(self).__name__)
+            self.delPressed.emit()
+            return event.ignore()
+
+        if isEsc:
+            print(f"Escape {text} pressionado, sinal emitido", type(self).__name__)
+            self.clearPressed.emit()
+            return event.ignore()
+
+        if isOperator:
+            if text.lower() == "p":
+                text = "^"
+            print(
+                f"OperadorPressed {text} pressionado, sinal emitido",
+                type(self).__name__,
+            )
+            self.operatorPressed.emit(text)
+            return event.ignore()
+
+        # Não passar daqui se não tiver texto
+        if isEmpty(text):
+            return event.ignore()
+
+        if isNumOrDot(text):
+            print(
+                f"inputPressed {text} pressionado, sinal emitido", type(self).__name__
+            )
+
+            self.inputPressed.emit(text)
+            return event.ignore()
+
+        print("Texto", text)
--- aulas/GUI/aula255calculadora/utils.py
+++ aulas/GUI/aula255calculadora/utils.py
@@ -22,3 +22,12 @@ def isValidNumber(string: str):
     except ValueError:
         valid = False
     return valid
+
+
+def converToNumber(string: str):
+    number = float(string)
+
+    if number.is_integer():
+        number = int(number)
+
+    return number
