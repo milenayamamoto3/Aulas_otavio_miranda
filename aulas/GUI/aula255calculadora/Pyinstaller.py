58ef3bf7e62aed1b6b8d914245fa8af0bc3c4c08
Milena Yamamoto <milymanamy3@gmail.com>
2024-06-13 20:31:34 +0900

    .

---
+++ aulas/GUI/aula255calculadora/Pyinstaller.py
@@ -0,0 +1,5 @@
+# Instalar o Pyinstaller
+# https://www.pythonguis.com/tutorials/packaging-pyside6-applications-windows-pyinstaller-installforge/
+# https://pyinstaller.org/en/stable/
+# python -m pip install pyinstaller
+# pyinstaller --name="Calculadora" --noconfirm --onefile --add-data="aula255calculadora/files/;files/" --icon="aula255calculadora/files/icon.png" --noconsole --clean --log-level=WARN aula255calculadora/main.py
