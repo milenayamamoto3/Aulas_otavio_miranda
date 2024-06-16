2c088a21b7aea3d945256357557796c0a8c2b528
Milena Yamamoto <milymanamy3@gmail.com>
2024-06-16 18:01:09 +0900

    .

--- aulas/GUI/aula256qtdesigner/src/main_window.py
+++ aulas/GUI/aula256qtdesigner/src/main_window.py
@@ -1,5 +1,8 @@
 import sys
 
+from typing import cast
+from PySide6.QtCore import QObject, QEvent
+from PySide6.QtGui import QKeyEvent
 from PySide6.QtWidgets import QApplication, QMainWindow
 from window import Ui_MainWindow
 
@@ -10,10 +13,22 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         self.buttonSend.clicked.connect(self.changeLabelResult)  # type: ignore
+        # self.lineName.setStyleSheet("background: red;")
+        self.lineName.installEventFilter(self)
 
     def changeLabelResult(self):
         text = self.lineName.text()
         self.labelResult.setText(text)
+
+    def eventFilter(self, watched: QObject, event: QEvent) -> bool:
+        # print(event.type()) # mostra todos os tipos de eventos que você faz
+        if event.type() == QEvent.Type.KeyPress:
+            # KeyPress: tecla pressionada
+            event = cast(QKeyEvent, event)  # muda tipagem do event
+            text = self.lineName.text()
+            self.labelResult.setText(text + event.text())
+
+        return super().eventFilter(watched, event)
 
 
 if __name__ == "__main__":
