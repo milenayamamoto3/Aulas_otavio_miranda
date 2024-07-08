e652fe8f3a0a677042792fd4b585eaf03ccd2373
Milena Yamamoto <milymanamy3@gmail.com>
2024-07-08 16:31:07 +0900

    .

---
+++ aulas/Ola_Django/django_info.py
@@ -0,0 +1,11 @@
+# criando um projecto :
+# dir com o "manage.py" inside
+# django-admin startproject nomedoProjeto
+# dir com o "manage.py" out
+# django-admin startproject nomedoProjeto .
+
+# deletando um projeto:
+# rmdir NomeDoProjeto
+
+# sair (no terminal):
+# crtl + c
